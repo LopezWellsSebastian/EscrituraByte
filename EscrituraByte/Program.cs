cf9a68c1ed919de66d8e7b60570f435f6428062f
LopezWellsSebastian <105455846+LopezWellsSebastian@users.noreply.github.com>
2022-06-06 11:16:09 -0700

    EscrituraByte
    Usamos el FileStream para escribir una frase y guardarlo en un texto

---
+++ EscrituraByte/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace EscrituraByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            byte[] buffer = new byte[81];
+            int nBytes=0, Car;
+            try
+            {
+                fs = new FileStream("text.txt", FileMode.Create, FileAccess.Write);
+                Console.WriteLine("Escriba el texto que desea almacenar en el archivo: ");
+
+                while ((Car=Console.Read()) != '\r' && (nBytes<buffer.Length))
+                {
+                    buffer[nBytes] = (byte)Car;
+                    nBytes++;
+                    
+                }
+                fs.Write(buffer, 0, nBytes);
+            }
+            catch(IOException E)
+            {
+                Console.WriteLine(E.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
